7cfce6f662ec7ff2b27315e5513a642b2e4a6e05
Ankeet Maini <ankeet.maini@gmail.com>
2021-02-15 23:18:18 +0530

    adds navigation

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import Amplify, {Auth, Hub} from 'aws-amplify';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
 import {withOAuth} from 'aws-amplify-react-native';
 import config from './aws-exports';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 async function urlOpener(url, redirectUrl) {
   await InAppBrowser.isAvailable();
@@ -27,22 +29,41 @@ Amplify.configure({
   },
 });
 
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>logged in </Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 const App = (props: any) => {
   const {oAuthUser, googleSignIn, signOut} = props;
 
   return (
-    <View>
-      <Text>
-        User: {oAuthUser ? JSON.stringify(oAuthUser.attributes) : 'None'}
-      </Text>
-      {oAuthUser ? (
-        <Button title="Sign Out" onPress={signOut} />
-      ) : (
-        <>
-          <Button title="Google" onPress={googleSignIn} />
-        </>
-      )}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {oAuthUser ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        )}
+      </Stack.Navigator>
+      <View>
+        <Text>
+          User: {oAuthUser ? JSON.stringify(oAuthUser.attributes) : 'None'}
+        </Text>
+        {oAuthUser ? (
+          <Button title="Sign Out" onPress={signOut} />
+        ) : (
+          <>
+            <Button title="Google" onPress={googleSignIn} />
+          </>
+        )}
+      </View>
+    </NavigationContainer>
   );
 };
 
