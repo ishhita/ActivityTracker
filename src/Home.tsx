c99a865a4b67ffa8ac7c2a661ef61aa4c8618623
Ankeet Maini <ankeet.maini@gmail.com>
2021-02-20 20:13:20 +0530

    Add api integration

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import API from '@aws-amplify/api';
+import React, {useEffect, useState} from 'react';
+import {Button, Text, View} from 'react-native';
 
 type Props = {
   email: string;
 };
 
 export default function Home(props: Props) {
+  const [user, setUser] = useState({});
+  const fetchUser = () => {
+    console.log(props.email);
+    API.get('activityStore', '/log/:activity', {})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  useEffect(() => {
+    fetchUser();
+  });
   return (
     <View>
       <Text>Hello, {props.email}</Text>
+      <Button onPress={fetchUser} title=" call api"></Button>
     </View>
   );
 }
