fabc677c91a66968e455e0a5b7ed954b169bf244
Ankeet Maini <ankeet.maini@gmail.com>
2021-02-21 11:48:16 +0530

    Add get/put for user profile

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,30 +1,42 @@
-import API from '@aws-amplify/api';
 import React, {useEffect, useState} from 'react';
 import {Button, Text, View} from 'react-native';
-
+import api from './API';
 type Props = {
   email: string;
 };
 
 export default function Home(props: Props) {
   const [user, setUser] = useState({});
-  const fetchUser = () => {
-    console.log(props.email);
-    API.get('activityStore', '/log/:activity', {})
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  const [message, setMessage] = useState('');
+  const fetchUser = async () => {
+    try {
+      const user = await api.get(`/user/${props.email}`);
+      console.log({user});
+      if (Object.keys(user).length === 0) {
+        setMessage('First time user, creating a profile...');
+        const response = await api.put(`/user/${props.email}`, {
+          pk: props.email,
+          sk: 'profile',
+          proDate: 0,
+          activities: [],
+          friends: [],
+        });
+        console.log('response', response);
+        setMessage('Profile created!');
+      }
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
   };
   useEffect(() => {
     fetchUser();
-  });
+  }, []);
   return (
     <View>
       <Text>Hello, {props.email}</Text>
-      <Button onPress={fetchUser} title=" call api"></Button>
+      <Text style={{color: 'blue', fontSize: 30, textAlign: 'center'}}>
+        {message}
+      </Text>
     </View>
   );
 }
